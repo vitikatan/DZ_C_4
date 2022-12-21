97126a7e5e661b5c1daccf6be2658ebd090f583e
Yana <wyemelt@gmail.com>
2022-12-21 21:49:19 +0300

    Zadacha_27 done

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear ();
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+int digit = 0;
+int sum = 0;
+
+while (number > 0)
+{
+    digit = number % 10;
+    sum = sum +digit;
+    number = number / 10;
+}
+Console.Write(sum);
