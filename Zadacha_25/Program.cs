ef9afe6b8198fe03b32ab8d123816ac2aad6b0a8
Yana <wyemelt@gmail.com>
2022-12-21 21:08:57 +0300

    Zadacha_25 done

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear ();
+Console.Write("Введите числа: ");
+
+double numberA = double.Parse(Console.ReadLine());
+double numberB = double.Parse(Console.ReadLine());
+double stepen = 1;
+while (stepen > 0)
+{
+    stepen = Math.Pow(numberA,numberB);
+    Console.WriteLine(stepen);
+    break;
+}
