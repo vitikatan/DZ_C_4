5cf91335a4120821da3656d05b0d3a9e1124bf87
Yana <wyemelt@gmail.com>
2022-12-21 22:23:30 +0300

    Zadacha_29 done

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear ();
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < 8; i++)
+   array[i] = rand.Next(0,100);
+var str = string.Join(" ", array);
+Console.WriteLine(str);
